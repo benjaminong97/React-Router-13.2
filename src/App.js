389a1aec8b4732b3859d95131727d5f968b4fc53
Benjamin Ong <89738593+benjaminong97@users.noreply.github.com>
2022-08-04 10:54:01 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <div id="page1">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+      <div id="page2">
+        <h1>Page 2</h1>
+      </div>
+      <div id="page3">
+        <h1>Page 3</h1>
+      </div>
+    </React.Fragment>
+  );
+}
